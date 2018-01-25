beb869283e92582a81b31ccd75947f986ed72938
DengSongsong <DengSongsong1010@163.com>
2018-01-25 10:48:38 +0800

    完成页面左侧导航

--- vue-posSystem/src/App.vue
+++ vue-posSystem/src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <!-- 左侧导航 -->
+    <leftNav></leftNav>
     <router-view/>
   </div>
 </template>
 
 <script>
+import leftNav from '@/components/common/leftNav'
 export default {
-  name: 'App'
+  name: 'App',
+  components:{
+    leftNav
+  }
 }
 </script>
 
@@ -18,6 +23,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ vue-posSystem/src/components/pages/Pos.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Pos</div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style>
+
+</style>
