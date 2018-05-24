f1572efe5b86089ef09c46ba2b52ea4a8b1177bb
DengSongsong <DengSongsong1010@163.com>
2018-05-24 21:46:58 +0800

    数据双向绑定

--- sourceCode/mvvm/js/mvvm.js
+++ sourceCode/mvvm/js/mvvm.js
@@ -1,30 +1,60 @@
 function MVVM(options) {
-  // 传入的参数
-  this.$options = options || {}
-  // console.log(this.$options)
+  // 传入的参数选项
+  this.$options = options || {};
   // data对象
-  let data = this._data = this.$options.data
-  // console.log(data)
-  let me = this
-  // Object.keys(data).forEach(key => {
-  //   this._proxyData(key)
-  // })
+  var data = this._data = this.$options.data;
+  var me = this;
 
+  // 数据代理
+  // 实现 vm.xxx -> vm._data.xxx
+  Object.keys(data).forEach(function(key) {
+      me._proxyData(key);
+  });
+
+  this._initComputed();
+
+  // 观测数据
+  observe(data, this);
+
+  this.$compile = new Compile(options.el || document.body, this)
 }
 
 MVVM.prototype = {
+  $watch: function(key, cb, options) {
+      new Watcher(this, key, cb);
+  },
+
+  // 数据代理
+  // 实现 vm.xxx -> vm._data.xxx
   _proxyData: function(key, setter, getter) {
-    let me = this
-    setter = setter || 
-    Object.defineProperty(me, key, {
-      configurable: false,
-      enumerable: true,
-      get: function proxyGetter() {
-        return me._data[key]
-      },
-      set: function proxySetter(newVal) {
-        me._data[key] = newVal
+      var me = this;
+      setter = setter || 
+      Object.defineProperty(me, key, {
+          configurable: false,
+          enumerable: true,
+          get: function proxyGetter() {
+              return me._data[key];
+          },
+          set: function proxySetter(newVal) {
+              me._data[key] = newVal;
+          }
+      });
+  },
+
+  _initComputed: function() {
+      var me = this;
+      var computed = this.$options.computed;
+      console.log(Object.keys(computed))
+      if (typeof computed === 'object') {
+          Object.keys(computed).forEach(function(key) {
+            console.log(computed[key].get)
+              Object.defineProperty(me, key, {
+                  get: typeof computed[key] === 'function' 
+                          ? computed[key] 
+                          : computed[key].get,
+                  set: function() {}
+              });
+          });
       }
-    })
   }
-}
+};
